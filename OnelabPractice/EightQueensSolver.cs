32bf380f5beb92ec3d23424f98cbbcc67628ae01
john <john.voo@mjmtech.com.tw>
2023-10-27 00:35:31 +0800

    first commit

---
+++ OnelabPractice/EightQueensSolver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnelabPractice
+{
+    public class EightQueensSolver
+    {
+        public static List<int[]> SolveQueens(int n)
+        {
+            List<int[]> solutions = new();
+            int[] board = new int[n];
+
+            SolveQueensUtil(n, 0, board, solutions);
+            return solutions;
+        }
+
+        private static void SolveQueensUtil(int n, int row, int[] board, List<int[]> solutions)
+        {
+            if (row == n)
+            {
+                int[] solution = new int[n];
+                Array.Copy(board, solution, n);
+                solutions.Add(solution);
+            }
+            else
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (IsSafe(board, row, col))
+                    {
+                        board[row] = col;
+                        SolveQueensUtil(n, row + 1, board, solutions);
+                        board[row] = -1;
+                    }
+                }
+            }
+        }
+
+        private static bool IsSafe(int[] board, int row, int col)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                if (board[i] == col || Math.Abs(board[i] - col) == Math.Abs(i - row))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ OnelabPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using OnelabPractice;
+
+int n = 8; 
+List<int[]> solutions = EightQueensSolver.SolveQueens(n);
+Console.WriteLine("Distinct solutions to the 8-queens puzzle:");
+PrintSolutions(solutions);
+
+
+static void PrintSolutions(List<int[]> solutions)
+{
+    foreach (var solution in solutions)
+    {
+        for (int i = 0; i < solution.Length; i++)
+        {
+            for (int j = 0; j < solution.Length; j++)
+            {
+                if (solution[i] == j)
+                {
+                    Console.Write("Q ");
+                }
+                else
+                {
+                    Console.Write(". ");
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
